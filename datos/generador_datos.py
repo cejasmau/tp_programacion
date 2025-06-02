c9433c76c5d2e7b8b2358f03b3759325fe638e1a
Mauro <mcejasmarcovecchio@gmail.com>
2025-06-02 16:59:00 -0300

    Incorporacion de computacion, cambio readme y nuevo grafico con subplots

--- datos/generador_datos.py
+++ datos/generador_datos.py
@@ -5,8 +5,9 @@ from faker import Faker
 def generar_csv_estudiantes(n_estudiantes: int = 20, nombre_archivo: str = 'estudiantes.csv') -> str:
     """Genera un archivo CSV con datos aleatorios de estudiantes y sus notas"""
     fake = Faker('es_ES')
-    materias = ['matemáticas', 'física', 'química', 'biología', 'literatura',
-                'historia', 'geografía', 'economía', 'inglés', 'artes', 'edu_fisica']
+    materias = ['matemáticas', 'física', 'química', 'biología', 
+                'literatura', 'historia', 'geografía', 'economía',
+                 'inglés', 'artes', 'educación_fisica', 'computación']
     
     with open(nombre_archivo, mode='w', encoding='utf-8', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=['nombre', 'apellido'] + materias)
